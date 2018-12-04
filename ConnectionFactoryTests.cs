b2611ffa597bb5e97cf6971e7e2456e58fdc699b
Concetto Condorelli <concettocondorelli@gmail.com>
2018-12-04 13:57:10 +0100

    Add tests for CreateConnection method

--- ConnectionFactoryTests.cs
+++ ConnectionFactoryTests.cs
@@ -1,4 +1,5 @@
 using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 using System;
 using Xunit;
 
@@ -7,29 +8,62 @@ namespace RabbitMQ.Learning.Tests
     public class ConnectionFactoryTests
     {
         [Fact]
-        public void GivenAFactoryWhenNoUriIsProvidedThenPropertiesShouldBeAsDefault()
+        public void GivenAFactoryThenPropertiesShouldBeAsDefault()
         {
-            var factory = new ConnectionFactory();
-
-            Assert.Equal("guest", factory.UserName);
-            Assert.Equal("guest", factory.Password);
-            Assert.Equal("localhost", factory.HostName);
-            Assert.Equal(-1, factory.Port);
-            Assert.Equal("/", factory.VirtualHost);
+            new TestBuilder<ConnectionFactory>()
+                .Given(() => new ConnectionFactory())
+                .Then(factory =>
+                {
+                    Assert.Equal("guest", factory.UserName);
+                    Assert.Equal("guest", factory.Password);
+                    Assert.Equal("localhost", factory.HostName);
+                    Assert.Equal(-1, factory.Port);
+                    Assert.Equal("/", factory.VirtualHost);
+                });
         }
 
         [Fact]
         public void GivenAFactoryWhenAnUriIsAssignedThenPropertiesShouldChangeAccordingly()
         {
-            var factory = new ConnectionFactory();
-
-            factory.Uri = new Uri("amqp://username:password@hostname:1234/vhost");
-
-            Assert.Equal("username", factory.UserName);
-            Assert.Equal("password", factory.Password);
-            Assert.Equal("hostname", factory.HostName);
-            Assert.Equal(1234, factory.Port);
-            Assert.Equal("vhost", factory.VirtualHost);
+            new TestBuilder<ConnectionFactory>()
+                .Given(() => new ConnectionFactory())
+                .When(factory => factory.Uri = new Uri("amqp://username:password@hostname:1234/vhost"))
+                .Then(factory =>
+                {
+                    Assert.Equal("username", factory.UserName);
+                    Assert.Equal("password", factory.Password);
+                    Assert.Equal("hostname", factory.HostName);
+                    Assert.Equal(1234, factory.Port);
+                    Assert.Equal("vhost", factory.VirtualHost);
+                });
         }
+
+        [Fact]
+        public void GivenAFactoryWhenHostIsUnreachableThenCreateConnectionShouldThrownAnException()
+        {
+            new TestBuilder<ConnectionFactory>()
+                .Given(() => new ConnectionFactory())
+                .When(factory => factory.HostName = "unknown-host")
+                .Then(factory =>
+                {
+                    Assert.Throws<BrokerUnreachableException>(() => factory.CreateConnection());
+                });
+        }
+
+        [Fact]
+        public void GivenAFactoryWhenHostIsReachableThenCreateConnectionShouldReturnAValidConnection()
+        {
+            new TestBuilder<ConnectionFactory>()
+                .Given(() => new ConnectionFactory())
+                .When(factory => factory.HostName = "localhost")
+                .Then(factory =>
+                {
+                    var connection = factory.CreateConnection();
+
+                    Assert.NotNull(connection);
+                    Assert.True(connection.IsOpen);
+                });
+        }
+
     }
 }
---
+++ TestBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RabbitMQ.Learning.Tests
+{
+    class TestBuilder<TContext>
+    {
+        private Func<TContext> _given;
+        private Action<TContext> _when;
+
+        public TestBuilder<TContext> Given(Func<TContext> given)
+        {
+            _given = given;
+            return this;
+        }
+
+        public TestBuilder<TContext> When(Action<TContext> when)
+        {
+            _when = when;
+            return this;
+        }
+
+        public void Then(Action<TContext> then)
+        {
+            TContext context = _given.Invoke();
+
+            _when?.Invoke(context);
+
+            then?.Invoke(context);
+        }
+    }
+}
