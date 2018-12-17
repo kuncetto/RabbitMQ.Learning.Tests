9a11ac5362517a5ec41754843767d2748585318e
Concetto Condorelli <concettocondorelli@gmail.com>
2018-12-17 12:00:43 +0100

    Add queue declaration tests

---
+++ RabbitMQ.Learning.Tests/Extensions/RandomExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace RabbitMQ.Learning.Tests.Extensions
+{
+    static class RandomExtensions
+    {
+        public static string NextString(this Random random, int length)
+        {
+            const int MaxAsciiDecimalValue = 126;
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                var n = random.Next(MaxAsciiDecimalValue);
+                sb.Append(Convert.ToChar(n));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RabbitMQ.Learning.Tests/QueueTests.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using RabbitMQ.Learning.Tests.Extensions;
+using System;
+using Xunit;
+
+namespace RabbitMQ.Learning.Tests
+{
+    public class QueueTests
+    {
+        [Fact]
+        public void DeclareAQueueWithAValidName()
+        {
+            var queueName = "rabbitmq.test.my_queue";
+            DeclareAQueueSuccessfully(queueName, queueName);
+        }
+
+        [Fact]
+        public void DeclareAServerNamedQueue()
+        {
+            var queueName = string.Empty;
+            DeclareAQueueSuccessfully(queueName, "amq.gen");
+        }
+
+        [Fact]
+        public void DeclareAQueueWithReservedAmqPrefixThrowsAnException()
+        {
+            var queueName = "amq.test.my_queue";
+            DeclareAQueueThrowsAnException<OperationInterruptedException>(queueName);
+        }
+
+        [Fact]
+        public void DeclareAQueueWithNameLongerThan255CharactersThrowsAnException()
+        {
+            var queueName = new Random().NextString(length: 256);
+            DeclareAQueueThrowsAnException<WireFormattingException>(queueName);
+        }
+
+        private void DeclareAQueueSuccessfully(string queueName, string expectedQueueName)
+        {
+            using (var connection = new ConnectionFactory { HostName = "localhost" }.CreateConnection())
+            {
+                new TestBuilder<IModel>()
+                    .Given(() => connection.CreateModel())
+                    .Then(context =>
+                    {
+                        var actual = context.QueueDeclare(queue: queueName);
+                        Assert.StartsWith(expectedQueueName, actual.QueueName);
+                    });
+            }
+        }
+
+        private void DeclareAQueueThrowsAnException<TException>(string queueName) where TException : Exception
+        {
+            using (var connection = new ConnectionFactory { HostName = "localhost" }.CreateConnection())
+            {
+                new TestBuilder<IModel>()
+                    .Given(() => connection.CreateModel())
+                    .Then(context =>
+                    {
+                        Assert.Throws<TException>(() => context.QueueDeclare(queue: queueName));
+                    });
+            }
+        }
+    }
+}
